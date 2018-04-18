3d54a69def467659bd482cada12e9f95eeb650cd
koast87 <branshaw2004@gmail.com>
2018-04-18 17:41:00 -0500

    relearning how to send to github

---
+++ TuesdayAssignment.java
@@ -0,0 +1,11 @@
+
+
+public class TuesdayAssignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("Brandon Crenshaw");
+	System.out.println("30");
+	}
+
+}
